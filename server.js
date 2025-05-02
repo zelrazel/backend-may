9d0d8fe5e39fa564aa224992e6db4d2faa871ef2
zel <zelblanc0712@gmail.com>
2025-05-03 02:41:46 +0800

    first commit

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const app = express();
 
 // CORS configuration
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
